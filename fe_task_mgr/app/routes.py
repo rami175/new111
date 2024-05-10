a2bdd0c751fff745971fb17fe90488019875da3d
Rami <ramicesar175@yahoo.com>
2024-05-10 12:25:45 -0700

    final report

--- fe_task_mgr/app/routes.py
+++ fe_task_mgr/app/routes.py
@@ -20,7 +20,7 @@ def about():
 
 @app.get("/tasks")
 def task_list():
-    response = requests.get(BACKEND_URL)
+    response = requests.get(BACKEND_URL) #essuing get request
     if response.status_code == 200:
         task_list = response.json().get("tasks")
         return render_template("list.html", tasks=task_list)
@@ -31,7 +31,7 @@ def task_list():
 
 @app.get("/tasks/<int:pk>")
 def detail(pk):
-    url = "%s/%s" % (BACKEND_URL, pk)
+    url = "%s/%s" % (BACKEND_URL, pk)#string formating
     response = requests.get(url)
     if response.status_code == 200:
          task = response.json().get("task")
@@ -40,3 +40,29 @@ def detail(pk):
         render_template("error.html", err=response.status_code),
         response.status_code
     )
+    
+@app.get("/tasks/edit/<int:pk>")
+def edit_task(pk):
+    url = "%s/%s" % (BACKEND_URL, pk)
+    response = requests.get(url)
+    if response.status_code == 200:
+         task = response.json().get("task")
+         return render_template("edit.html", task=task)
+    return(
+        render_template("error.html", err=response.status_code),
+        response.status_code
+    )
+    
+
+@app.post("/tasks/edit/<int:pk>")
+def submit_task_update(pk):
+    url = "%s/%s" % (BACKEND_URL, pk)
+    task_data = request.form
+    response =requests.put(url,json=task_data)
+    if response.status_code == 204:
+        return render_template("success.html", msg="Task updated successfully.")
+    return(
+        render_template("error.html", err=response.status_code),
+        response.status_code
+    )
+        
