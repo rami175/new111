6d336d88fbd6a335fa9a7b015234c4f863e8c5e8
Rami <ramicesar175@yahoo.com>
2024-05-20 08:16:06 -0700

    114

--- issue_mgr/issues/views.py
+++ issue_mgr/issues/views.py
@@ -12,13 +12,26 @@ from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Issue, Status
 
-#scrum master's view
-class IssueListView(LoginRequiredMixin, UserPassesTestMixin, ListView):
-    template_name = "issues/list.html"
+#
+class boardView(LoginRequiredMixin, UserPassesTestMixin, ListView):
+    template_name = "issues/board.html"
     model = Issue
     
-    def test_func(self):
-        return  self.request.user.role == "scrum master"
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        to_do = Status.objects.get(name="to do")
+        in_progress = Status.objects.get(name="in progress")
+        done = Status.objects.get(name="done")
+        context["to_do_list"] = Issue.objects.filter(
+            status = to_do).order_by("created_on").reverse()
+        context["in_progress_list"] = Issue.objects.filter(
+            status = in_progress).order_by("created_on").reverse()
+        context["done_list"] = Issue.objects.filter(
+            status = done).order_by("created_on").reverse()
+        
+        return context
+    
     
 
 # any team member can view issues (Phase II 1 & 1.1)
@@ -45,7 +58,7 @@ class IssueCreatelView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     fields = ["name", "summary", "description", "status"]
     
     def test_func(self):
-        return  self.request.user.role == "product owner"
+        return  self.request.user.role.name == "product owner"
     
     def form_valid(self, form):              
         form.instance.reporter = self.request.user
